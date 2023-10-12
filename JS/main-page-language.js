7e4db24296ae402521c26e9227c715678bd9a1ef
Sulaiman Sulaiman <118593711+Sulaiman-SAMM@users.noreply.github.com>
2023-10-12 10:28:30 +0200

    file uplod

---
+++ JS/main-page-language.js
@@ -0,0 +1,179 @@
+
+const translations = {
+    de: {
+        home:"Home",
+        services:"Dienstleistungen",
+        about:"Über uns",
+        contact:"Kontakt",
+        mainNamme:"Kurze-Info",
+        mainPargraph:"Willkommen bei K-I. Hier finden Sie kurze Informationen zu wichtigen Themen in Deutschland",
+        electricity:"Strom",
+        healthInsurance:"Krankenversicherung",
+        lawyer:"Anwälte",
+        auxiliaryCenters:"Hilfezentrum",
+        learnGerman:"Deutsch lernen",
+        dualStudy:"Ausbildung",
+        work:"Arbeit",
+        apartment:"Wohnung",
+        cars:"Auto",
+        vacation:"Urlaub",
+        party:"Partys in Deutschland",
+        interview:"Vorstellungsgespräch",
+        apps:"In Deutschland verwendete APPs",
+        books:"Buchseiten",
+        contracts:"Arbeitsverträge",
+        school:"Schulsystem A bis Z",
+        financial:"finanzielle Probleme",
+        bank:"Bank",
+        please:"Hinterlassen Sie uns gerne Ihre Anregungen unter:",
+        aboutX2:"Über Uns",
+        pragraf:"Als jemand, der die Menschen,die in Deutschland leben und unter ihrem demokratischenSystem stehen, unterstützen möchte, strebe ich danach,Lösungen für die Herausforderungen zu finden, mit denen Menschen in ihrem Leben konfrontiert sind. Dies betrifft Bereiche wie Arbeit, Wohnen, Gesundheit, Familie und Integrationsprozesse, und vieles mehr",
+    },
+    ar: {
+        home:"الصفحه الرئيسيه",
+        services:"خدمات",
+        about:"من نحن",
+        contact:"تواصل معنا",
+        mainNamme:"معلومة مقتطفة",
+        mainPargraph:"مرحبًا بك في صفحة معلومة مقتطفة. ستجد هنا معلومات مختصرة عن مواضيع مهمة في ألمانيا",
+        healthInsurance:"تأمين الصحي",
+        electricity:"الكهرباء",
+        lawyer:"محاماه",
+        auxiliaryCenters:"المراكز المساعدة",
+        learnGerman:"تعلم الالمانية",
+        dualStudy:"الدراسه المزدوجه",
+        work:"العمل",
+        apartment:"منازل",
+        cars:"سيارات",
+        vacation:"أجازة",
+        party:"احزاب في المانيا",
+        interview:"مقابلات عمل",
+        apps:"تطبيقات مستخدمه في المانيا",
+        books:"مواقع بيع الكتب",
+        contracts:"عقود عمل",
+        school:"نظام الدراسي",
+        financial:"مشاكل ماليه",
+        bank:"البنوك",
+        please:"لا تتردد في ترك لنا مقترحاتك على:",
+        aboutX2:"من نحن",
+        pragraf:"كشخص محب لمساعده الأشخاص الذين يعيشون على أراضي ألمانيا وتحت منظومتها الديمقراطية، أرغب في المشاركة في ايجاد حلول للتحديات التي يواجهها الإنسان في حياته، سواء في مجال العمل، أو السكن، أو الصحة، أو الأسرة، أو عملية الاندماج، وما إلى ذلك.",
+    },
+    en: {
+        home:"Home",
+        services:"Services",
+        about:"About",
+        contact:"contact",
+        mainNamme:"Short-Info",
+        mainPargraph:"Welcome to K-I. Here you will find brief information on important topics in Germany",
+        electricity:"Electricity",
+        healthInsurance:"Health Insurance",
+        lawyer:"Lawyer",
+        auxiliaryCenters:"Auxiliary Centers",
+        learnGerman:"Learn German",
+        dualStudy:"Dual study",
+        work:"Work",
+        apartment:"Apartments",
+        cars:"Cars",
+        vacation:"Vacation",
+        party:"Parties in Germany",
+        interview:"Job interviews",
+        apps:"Applications used in Germany",
+        books:"Book selling sites",
+        contracts:"Job Contracts",
+        school:"School system",
+        financial:"Financial problems",
+        bank:"Banks",
+        please:"Feel free to leave us your suggestions at:",
+        aboutX2:"About",
+        pragraf:"As someone who is passionate about assisting individuals residing within the territory of Germany and under its democratic system, I aspire to contribute to finding solutions for the challenges that people face in their lives. This includes areas such as employment, housing, healthcare, family, integration processes, and more.",
+    },
+    pe: {
+        home:"صفحه اصلی",
+        services:"خدمات",
+        about:"درباره ما",
+        contact:"مخاطب",
+        mainNamme:"اطلاعات کوتاه",
+        mainPargraph:"به K-I خوش آمدید. در اینجا اطلاعات مختصری در مورد موضوعات مهم در آلمان خواهید یافت",
+        electricity:"برق",
+        healthInsurance:"بیمه سلامت",
+        lawyer:"وکلا",
+        auxiliaryCenters:"مرکز کمک",
+        learnGerman:"آلمانی یاد بگیر",
+        dualStudy:"مطالعه دوگانه",
+        work:"کار کنید",
+        apartment:"اپارتمان",
+        cars:"ماشین ها",
+        vacation:"تعطیلات",
+        party:"احزاب در آلمان",
+        interview:"مصاحبه شغلی",
+        apps:"برنامه های مورد استفاده در آلمان",
+        books:"خرید آنلاین کتاب",
+        contracts:"قراردادهای کار",
+        school:"مدرسه سیستم",
+        financial:"مشکلات مالی",
+        bank:"بانک",
+        please:"لطفا پیشنهادات خود را در آدرس زیر با ما در میان بگذارید:",
+        aboutX2:"درباره ما",
+        pragraf:"به عنوان کسی که می‌خواهد افرادی که در آلمان زندگی می‌کنند و تحت نظام دموکراتیک آن هستند را حمایت کند، تلاش می‌کنم تا به یافتن راه‌حل‌هایی برای چالش‌هایی که افراد در زندگی خود با آن مواجه هستند بپردازم. این شامل حوزه‌هایی مانند کار، مسکن، بهداشت، خانواده و فرآیندهای ادغام و موارد دیگر می‌شود.",
+
+    },
+    uk: {
+        home:"додому",
+        services:"Послуги",
+        about:"про нас",
+        contact:"контакт",
+        mainNamme:"короткий-інформація",
+        mainPargraph:"Ласкаво просимо до K-I. Тут ви знайдете коротку інформацію про важливі теми в Німеччині",
+        electricity:"електрика",
+        healthInsurance:"Медична страховка",
+        lawyer:"юристи",
+        auxiliaryCenters:"Центр допомоги",
+        learnGerman:"вивчати німецьку мову",
+        dualStudy:"дуальне навчання",
+        work:"працювати",
+        apartment:"квартира",
+        cars:"авто",
+        vacation:"відпустка",
+        party:"Вечірки в Німеччині",
+        interview:"співбесіда",
+        apps:"APP, що використовуються в Німеччині",
+        books:"Сторінки книги",
+        contracts:"трудові договори",
+        school:"шкільна система",
+        financial:"фінансові проблеми",
+        bank:"банк",
+        please:"Будь ласка, залишайте нам свої пропозиції за адресою:",
+        aboutX2:"про нас",
+        pragraf:"Як людина, яка бажає допомогти людям, які проживають в Німеччині і перебувають під її демократичною системою, я прагну знаходити рішення для викликів, з якими стикаються люди в своєму житті. Це стосується таких сфер, як праця, житло, охорона здоров'я, сім'я, процеси інтеграції та багато іншого.",
+    },
+};
+
+const languageSelector = document.querySelector("select");
+languageSelector.addEventListener("change", (event) => {
+    setLanguage(event.target.value);
+    localStorage.setItem("lang", event.target.value);
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const language = localStorage.getItem("lang");
+    setLanguage(language);
+});
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll("[data-lang]");
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute("data-lang");
+        element.textContent = translations[language][translationKey];
+    });
+
+    // document.dir = language === "ar"  ? "rtl" : "ltr";
+
+
+
+    // document.dir = language === "pe" ? "rtl" : "ltr";
+
+    // if (language === "ar") {
+    //     document.dir = "rtl";
+    // } else {
+    //     document.dir = "ltr";
+    // }
+};
